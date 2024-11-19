06b3c1c7e57998d9530da227842466b52a593999
lifeisfun19 <165129019+lifeisfun19@users.noreply.github.com>
2024-11-19 11:51:30 +0530

    Update app.py

--- app.py
+++ app.py
@@ -40,6 +40,5 @@ def predict():
         return jsonify({'prediction': prediction[0]})
     except Exception as e:
         return jsonify({'error': str(e)}), 500
-
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
